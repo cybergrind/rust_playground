0bdf7c864aa1757de81497b2a8e9965099867574
Kirill Pinchuk <cybergrind@gmail.com>
2021-08-12 16:36:24 +0300

    jump_game: initial solution

---
+++ src/leetcode/jump_game.rs
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/jump-game/
+pub struct Solution {}
+
+impl Solution {
+    pub fn can_jump(nums: Vec<i32>) -> bool {
+        let mut max_i:i32 = 0;
+        for (idx, val) in nums.iter().enumerate() {
+            if idx as i32 > max_i {
+                return false;
+            }
+            max_i = std::cmp::max(*val + idx as i32, max_i);
+        }
+        return true;
+    }
+}
+
+// submission codes end
+// cargo test test_55
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_55() {
+        assert_eq!(Solution::can_jump(vec![2, 3, 1, 1, 4]), true);
+        assert_eq!(Solution::can_jump(vec![3, 2, 1, 0, 4]), false);
+        assert_eq!(Solution::can_jump(vec![2, 3, 1, 1, 0, 0, 0, 4]), false);
+        assert_eq!(Solution::can_jump(vec![8, 3, 1, 1, 0, 0, 0, 4]), true);
+        assert_eq!(Solution::can_jump(vec![0]), true);
+        assert_eq!(Solution::can_jump(vec![1, 1, 2, 2, 0, 1, 1]), true);
+        assert_eq!(
+            Solution::can_jump(vec![1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0]),
+            true
+        );
+    }
+}
